5aeb588aceec85e18a9aa240527f0ed8c9aa932f
Osmar Martinez <osmar_osmg@hotmail.com>
2026-03-30 18:33:02 -0600

    [fix] auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -126,7 +126,7 @@ export class AuthController {
   })
   async loginPin(
     @Body() dto: LoginAuthPinDto,
-    @Query('Nombres') nombres: string | undefined,
+    @Query('Nombres') nombres: string | 'NXT',
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ) {
